ea6624074b60822622773b4d5025ed681b8a3313
dazai19090619 <mirai20080609@icloud.com>
2026-05-12 13:52:56 +0900

    ユーザー登録・ログイン機能の実装（バリデーション追加、登録/ログイン画面修正、ヘッダーのログイン分岐実装)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  KATAKANA_REGEX = /\A[ァ-ヶー]+\z/
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :first_name, format: { with: FULL_WIDTH_REGEX }
+    validates :last_name, format: { with: FULL_WIDTH_REGEX }
+    validates :first_name_kana, format: { with: KATAKANA_REGEX }
+    validates :last_name_kana, format: { with: KATAKANA_REGEX }
+  end
+
+  validates :password,
+            format: { with: PASSWORD_REGEX, message: "は半角英数字混合で入力してください" }
 end
