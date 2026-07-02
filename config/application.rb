64800fe0654a34fc126c131318c154873e5b5394
dazai19090619 <mirai20080609@icloud.com>
2026-07-02 20:08:58 +0900

    商品出品機能の実装（投稿・バリデーション）

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,9 @@
-require_relative "boot"
-
-require "rails/all"
-
-Bundler.require(*Rails.groups)
-
 module Furima41273
   class Application < Rails::Application
     config.load_defaults 7.1
 
     config.autoload_lib(ignore: %w(assets tasks))
 
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
   end
 end
