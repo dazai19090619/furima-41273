0c2b08f96932da2bb6ea349d694baf036e4fe9aa
dazai19090619 <mirai20080609@icloud.com>
2026-07-09 13:16:12 +0900

    商品詳細表示機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -17,6 +18,10 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
